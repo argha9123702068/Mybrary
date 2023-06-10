706ce7292a122957239b053917715a44a29daecf
argha9123792068 <arghapal11@gmail.com>
2023-06-10 20:57:56 +0530

    code added

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authoreSchema = new mongoose.Schema({
     name:{
@@ -8,4 +9,15 @@ const authoreSchema = new mongoose.Schema({
     }
 })
 
+authoreSchema.pre("deleteOne", function(next){
+     Book.find({author:this.id},(err,books)=>{
+        if(err){
+            next(err)
+        }else if (books.length>0){
+            next(new Error("This author still has books"))
+        }else{
+            next()
+        }
+    })
+})
 module.exports=mongoose.model('Author',authoreSchema)
